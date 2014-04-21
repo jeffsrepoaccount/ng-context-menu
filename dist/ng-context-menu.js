9876b971b487433badfc29ede50a8e8e3ed0b4c1
jeff <jefflikeschicken@gmail.com>
2014-04-21 15:42:51 -0400

    Adding contextMenu service, updates for style guide

--- dist/ng-context-menu.js
+++ dist/ng-context-menu.js
@@ -1,62 +1,133 @@
 /**
- * ng-context-menu - v0.0.9 - An AngularJS directive to display a context menu when a right-click event is triggered
+ * ng-context-menu - An AngularJS directive to display a context menu when a right-click event is triggered
  *
  * @author Ian Kennington Walter (http://ianvonwalter.com)
  */
 angular
   .module('ng-context-menu', [])
-  .directive('contextMenu', ['$window', '$parse', function($window, $parse) {
+  
+  /**
+   * Context menu service allows control of other context menus when another
+   * one is triggered, i.e. closing any other currently open context menu 
+   * when another one is opened if multiple context menus exist.
+   */
+  .service('contextMenu', function() {
+    var menus = [],
+        closeMenu,
+        openMenu;
+
+    //  {{{ contextMenu.closeMenu()
+
+    /**
+     * closeMenu() closes the given menu element.
+     *
+     * @param <Element> menuElement
+     */
+    closeMenu = function(menuElement) {
+      menuElement.removeClass('open');
+
+      // Set menuItem.isOpen to false
+      menus.forEach(function(menuItem) {
+        if(menuItem.element === menuElement) {
+          menuItem.isOpen = false;
+
+          // TODO: Break out of angular forEach?
+        }
+      });
+    };
+
+    // }}}
+    // {{{ contextMenu.openMenu()
+
+    /**
+     * openMenu() opens the given menu element, triggered by the 
+     * specified event.
+     *
+     * @param <Event> event
+     * @param <Element> menuElement
+     */
+    openMenu = function(event, menuElement) {
+      // Item that needs to be opened
+      var curMenu;
+
+      // Close all other opened menus, mark the current one
+      menus.forEach(function(menuItem) {
+        if(menuItem.element === menuElement) {
+          curMenu = menuItem;
+        } else if(menuItem.isOpen) {
+
+          // Close other menu
+          closeMenu(menuItem.element);
+          menuItem.isOpen = false;
+        }
+      });
+
+      if(!curMenu) {
+
+        // Menu does not exist yet. Create menu and push onto list
+        curMenu = {
+          isOpen: false,
+          element: menuElement
+        };
+        menus.push(curMenu);
+      }
+
+      if(!curMenu.isOpen) {
+
+        // Show element
+        // TODO: Fix for child element padding
+        menuElement.css('top', event.offsetY + 'px');
+        menuElement.css('left', event.offsetX + 'px');
+        menuElement.addClass('open');
+        curMenu.isOpen = true;
+      }
+    };
+
+    //  }}}
+
+    // Expose for outside service calls
+    this.closeMenu = closeMenu;
+    this.openMenu = openMenu;
+  })
+  .directive('contextMenu', ['$window', '$parse', 'contextMenu', function($window, $parse, contextMenu) {
     return {
       restrict: 'A',
       link: function($scope, element, attrs) {
-        var opened = false,
-          openTarget,
-          disabled = $scope.$eval(attrs.contextMenuDisabled),
-          win = angular.element($window),
-          menuElement = null,
-          fn = $parse(attrs.contextMenu);
-
-        function open(event, element) {
-          element.addClass('open');
-          element.css('top', Math.max(event.pageY, 0) + 'px');
-          element.css('left', Math.max(event.pageX, 0) + 'px');
-          opened = true;
-        }
-
-        function close(element) {
-          opened = false;
-          element.removeClass('open');
-        }
+        
+        var disabled = $scope.$eval(attrs.contextMenuDisabled),
+            win = angular.element($window),
+            fn = $parse(attrs.contextMenu),
+            menuElement,
+            openTarget;
 
         element.bind('contextmenu', function(event) {
+          
           if (!disabled) {
-            // Make sure the DOM is set before we try to find the menu
-            if (menuElement === null) {
+            if(menuElement === undefined) {
               menuElement = angular.element(document.getElementById(attrs.target));
             }
-
             openTarget = event.target;
             event.preventDefault();
             event.stopPropagation();
             $scope.$apply(function() {
               fn($scope, { $event: event });
-              open(event, menuElement);
+              contextMenu.openMenu(event, menuElement);
             });
           }
         });
 
         win.bind('keyup', function(event) {
-          if (!disabled && opened && event.keyCode === 27) {
+          if (menuElement && !disabled && event.keyCode === 27) {
             $scope.$apply(function() {
-              close(menuElement);
+              contextMenu.closeMenu(menuElement);
             });
           }
         });
 
         function handleWindowClickEvent(event) {
-          if (!disabled && opened && (event.button !== 2 || event.target !== openTarget)) {
+          if (menuElement && !disabled && (event.button !== 2 || event.target !== openTarget)) {
             $scope.$apply(function() {
-              close(menuElement);
+              contextMenu.closeMenu(menuElement);
             });
           }
         }
